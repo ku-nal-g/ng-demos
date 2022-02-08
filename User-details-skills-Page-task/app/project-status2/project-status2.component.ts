1f57abbd491e7f73e817b0545eb07e96b5bee87d
ku-nal-g <98161650+ku-nal-g@users.noreply.github.com>
2022-02-08 13:41:00 +0530

    Add files via upload

---
+++ User-details-skills-Page-task/app/project-status2/project-status2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-status2',
+  templateUrl: './project-status2.component.html',
+  styleUrls: ['./project-status2.component.css']
+})
+export class ProjectStatus2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
