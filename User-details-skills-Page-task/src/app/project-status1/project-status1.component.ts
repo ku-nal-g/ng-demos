b3cc3207b6c5262553a964247a380defe3170f7f
kunalg <kunal.g@codestoresolutions.com>
2022-02-08 16:47:07 +0530

    "Designed a User details web page using Angular"

---
+++ User-details-skills-Page-task/src/app/project-status1/project-status1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-status1',
+  templateUrl: './project-status1.component.html',
+  styleUrls: ['./project-status1.component.css']
+})
+export class ProjectStatus1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
